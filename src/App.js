76c997445441229d88d8c25257fc67bdb8c10e43
Pooria <p.azizi834@gmail.com>
2024-09-09 13:16:57 +0330

    changing persian font.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,6 @@
-/**
-=========================================================
-* Material Dashboard 2 React - v2.2.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/material-dashboard-react
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
-
-Coded by www.creative-tim.com
-
- =========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
 import { useState, useEffect, useMemo } from "react";
+import "./styles/css/fonts.css";
+import "./styles/css/styles.css";
 
 // react-router components
 import { Routes, Route, Navigate, useLocation } from "react-router-dom";
